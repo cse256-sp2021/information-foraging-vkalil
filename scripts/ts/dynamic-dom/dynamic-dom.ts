8d98cdf6f6f66c397325f6a72f43f619c4fddae8
vkalil <vkalil@wustl.edu>
2021-03-28 23:58:17 -0400

    last commits

--- scripts/ts/dynamic-dom/dynamic-dom.ts
+++ scripts/ts/dynamic-dom/dynamic-dom.ts
@@ -10,9 +10,36 @@ export function PutStudentPageLoadOperationsInsideThisStudentBody() {
     // TODO: Put all operations that you want to happen on ever page load in this function.
     // For example you could write: Sticky.setup()
     doSomething();
-    //stickToIt();
+    sticky();
+    //\\stickToIt();
 }
-
+export function sticky(){
+    window.onscroll = function() {myFunction()};
+  
+  // Get the header
+  var header = document.getElementById("main-menu-container");
+  
+  // Get the offset position of the navbar
+  if(header === null){
+      return
+  }
+  var sticky = header.offsetTop;
+  
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if(header === null){
+        return
+    }
+    console.log("running");
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+      console.log("add sticky");
+    } else {
+      header.classList.remove("sticky");
+      console.log("no need");
+    }
+  }
+}
 export async function setupAll() {
     await new Promise((r: any) => setTimeout(r, 100));
     console.log('reloading');
